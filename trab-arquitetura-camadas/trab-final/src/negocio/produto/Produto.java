c47ac88a3595b87e6434ff0b74ca129e1a76c12e
PauloOyama <57512581+PauloOyama@users.noreply.github.com>
2021-06-18 20:19:58 -0300

    Produto negocios

--- trab-arquitetura-camadas/trab-final/src/negocio/produto/Produto.java
+++ trab-arquitetura-camadas/trab-final/src/negocio/produto/Produto.java
@@ -4,61 +4,73 @@ import java.util.StringTokenizer;
 
 import dados.Gravavel;
 
-public class Produto implements Gravavel{
-    static final String SEPARADOR = "#";
-	
+public class Produto implements Gravavel {
+	static final String SEPARADOR = "#";
+
+	private float valor;
 	private String descricao;
 	private String codigo;
 	private int id;
 
-    public String getDescricao() {
+	public String getDescricao() {
 		return descricao;
 	}
 
-    public void setDescricao(String nome) {
+	public void setDescricao(String nome) {
 		this.descricao = nome;
 	}
 
-    public String getCodigo() {
+	public String getCodigo() {
 		return codigo;
 	}
 
-    public void setCodigo(String codigo) {
+	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
 
-    public int getId() {
+	public int getId() {
 		return id;
 	}
 
-    public void setId(int id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
-    @Override
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+
+	@Override
 	public String getRegistro() {
-		
+
 		StringBuffer reg = new StringBuffer();
 		reg.append(this.getId());
 		reg.append(SEPARADOR);
 		reg.append(this.getCodigo());
 		reg.append(SEPARADOR);
 		reg.append(this.getDescricao());
+		reg.append(SEPARADOR);
+		reg.append(this.getValor());
 		reg.append("\n");
-		
+
 		return reg.toString();
 	}
 
-    @Override
+	@Override
 	public void setRegistro(String reg) {
-		StringTokenizer st = new StringTokenizer(reg,SEPARADOR);
+		StringTokenizer st = new StringTokenizer(reg, SEPARADOR);
 		this.setId(Integer.parseInt(st.nextToken()));
 		this.setCodigo(st.nextToken());
 		this.setDescricao(st.nextToken());
+		this.setValor(Float.parseFloat(st.nextToken()));
 	}
 
-    @Override
+	@Override
 	public String toString() {
-		return "Cliente [id=" + id +", cpf=" + codigo + ", nome=" + descricao +  "]";
+		return "Cliente [id=" + id + ", cpf=" + codigo + ", nome=" + descricao + ", valor=" + valor + "]";
 	}
 }
